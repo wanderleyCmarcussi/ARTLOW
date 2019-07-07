c51d07f5e4c370c9ea8f4f6c2ca4638e36d012a8
Wanderley Marcussi <wmarcussi@me.com>
2019-07-07 19:17:33 -0300

    comit de 07 07

---
+++ VendasArtlow/VendasArtlow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendasArtlow
+//
+//  Created by Wanderley Marcussi on 03/06/19.
+//  Copyright © 2019 Wanderley Marcussi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
